e175d3dbbdcb7ff6e95f3e0c6a80d8d458ad737e
ESRAILHAQUE <esrailbblhs@gmail.com>
2024-05-10 16:42:57 +0600

    read data using id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -28,6 +28,12 @@ async function run() {
           res.send(cursor);
 
       })
+      app.get("/allfoods/:id", async (req, res) => {
+          const id = req.params.id;
+          const query={_id: new ObjectId(id)}
+         const result = await foodCollection.findOne(query)
+         res.send(result);
+       });
     
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
